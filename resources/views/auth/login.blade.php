3c0a0f48869d55e66b715ad4bb0f8ea05f1b824b
anhdao1401 <96332432+anhdao1401@users.noreply.github.com>
2021-12-26 14:25:26 +0700

    Update login.blade.php
    Đổi thành ngôn ngữ tiếng việt thôi hà

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">Username</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Tên đăng nhập</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="text" class="form-control @error('password') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="username" autofocus>
